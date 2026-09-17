f0b4cb67a5e1a8cc4ddf9cc28733b832b14e2c36
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-09-17 10:22:37 +0300

    feat: added ogImage versioning

--- site/salmon/src/routes/+layout.svelte
+++ site/salmon/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 	import image from '$lib/assets/hero.png'
+    import {PUBLIC_SITE_VERSION} from '$env/static/public'
+    const ofImage = `${image}?v=${PUBLIC_SITE_VERSION}`
 
 	let { children } = $props();
 </script>
@@ -14,7 +16,7 @@
 	<!-- Open Graph tags for WhatsApp, Facebook, LinkedIn, etc. -->
 	<meta property="og:title" content="Salmon — A Super Awesome Ledger" />
 	<meta property="og:description" content="A Super Awesome ledger" />
-	<meta property="og:image" content={image} />
+	<meta property="og:image" content={ofImage} />
 	<!-- <meta property="og:url" content="https://your-domain.com" /> -->
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="Salmon" />
@@ -23,6 +25,6 @@
 	<meta name="twitter:card" content="salmon logo" />
 	<meta name="twitter:title" content="Salmon — A Super Awesome Ledger" />
 	<meta name="twitter:description" content="A Super Awesome ledger" />
-	<meta name="twitter:image" content={image} />
+	<meta name="twitter:image" content={ofImage} />
 </svelte:head>
 {@render children()}
